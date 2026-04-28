a031a346c75fb8b131c53493f95e7c3e981d3582
JoelMatuso <matusojm246@gmail.com>
2026-04-28 21:03:48 +0200

    Add files via upload

---
+++ DiningOffers/DiningOffers/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿using System;
+
+namespace DiningFullSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Burger");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Pizza");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Steak");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Meals", new System.Windows.Forms.TreeNode[] {
+            treeNode7,
+            treeNode8,
+            treeNode9});
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Drinks");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Desserts");
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBoxCategories = new System.Windows.Forms.GroupBox();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.Item = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Price = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel3.SuspendLayout();
+            this.groupBoxCategories.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Lime;
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(803, 58);
+            this.panel3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(347, 9);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(136, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dining ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBoxCategories
+            // 
+            this.groupBoxCategories.Controls.Add(this.treeView2);
+            this.groupBoxCategories.Location = new System.Drawing.Point(12, 75);
+            this.groupBoxCategories.Name = "groupBoxCategories";
+            this.groupBoxCategories.Size = new System.Drawing.Size(242, 284);
+            this.groupBoxCategories.TabIndex = 4;
+            this.groupBoxCategories.TabStop = false;
+            this.groupBoxCategories.Text = "groupBoxCategories";
+            this.groupBoxCategories.UseWaitCursor = true;
+            // 
+            // treeView2
+            // 
+            this.treeView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView2.Location = new System.Drawing.Point(3, 22);
+            this.treeView2.Name = "treeView2";
+            treeNode7.Name = "Node1";
+            treeNode7.Text = "Burger";
+            treeNode8.Name = "Node3";
+            treeNode8.Text = "Pizza";
+            treeNode9.Name = "Node4";
+            treeNode9.Text = "Steak";
+            treeNode10.Name = "Node0";
+            treeNode10.Text = "Meals";
+            treeNode11.Name = "Node7";
+            treeNode11.Text = "Drinks";
+            treeNode12.Name = "Node8";
+            treeNode12.Text = "Desserts";
+            this.treeView2.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode10,
+            treeNode11,
+            treeNode12});
+            this.treeView2.Size = new System.Drawing.Size(236, 259);
+            this.treeView2.TabIndex = 0;
+            this.treeView2.UseWaitCursor = true;
+            this.treeView2.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView2_AfterSelect);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView2);
+            this.groupBox1.Location = new System.Drawing.Point(260, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 331);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBoxMenu";
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Item,
+            this.Price});
+            this.listView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView2.FullRowSelect = true;
+            this.listView2.GridLines = true;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(3, 22);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(283, 306);
+            this.listView2.TabIndex = 0;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // Item
+            // 
+            this.Item.Text = "Item";
+            this.Item.Width = 150;
+            // 
+            // Price
+            // 
+            this.Price.Text = "Price";
+            this.Price.Width = 100;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.lblTotal);
+            this.groupBox2.Controls.Add(this.listView3);
+            this.groupBox2.Location = new System.Drawing.Point(555, 75);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(236, 331);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBoxOrder";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(6, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Checkout";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.Location = new System.Drawing.Point(6, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add Item";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.BackColor = System.Drawing.Color.White;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(6, 212);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(79, 20);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "Total: $0";
+            this.lblTotal.Click += new System.EventHandler(this.lblTotal_Click);
+            // 
+            // listView3
+            // 
+            this.listView3.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(6, 22);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(224, 221);
+            this.listView3.TabIndex = 0;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Item";
+            this.columnHeader1.Width = 140;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Price";
+            this.columnHeader2.Width = 90;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 378);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Select Table";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Table 1",
+            "Table 2",
+            "Table 3"});
+            this.comboBox1.Location = new System.Drawing.Point(119, 375);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(255, 412);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(304, 23);
+            this.progressBar1.TabIndex = 9;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.IndianRed;
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 443);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(803, 32);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "Item adde";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(109, 25);
+            this.toolStripStatusLabel1.Text = "Item added!";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(803, 475);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBoxCategories);
+            this.Controls.Add(this.panel3);
+            this.Name = "Form1";
+            
+            
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.groupBoxCategories.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBoxCategories;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader Item;
+        private System.Windows.Forms.ColumnHeader Price;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ DiningOffers/DiningOffers/Form1.cs
@@ -0,0 +1,116 @@
+﻿// =============================
+// Form1.cs (MAIN LOGIC)
+// =============================
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DiningFullSystem
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, double> menu = new Dictionary<string, double>()
+{
+    {"Burger", 5.00},
+    {"Pizza", 8.50},
+    {"Steak", 12.00},
+    {"Juice", 2.50},
+    {"Ice Cream", 3.00}
+};
+
+        double total = 0;
+
+        private void lblTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView2_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            listViewMenu.Items.Clear();
+
+            if (menu.ContainsKey(e.Node.Text))
+            {
+                ListViewItem item = new ListViewItem(e.Node.Text);
+                item.SubItems.Add(menu[e.Node.Text].ToString("0.00"));
+                listViewMenu.Items.Add(item);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (listViewMenu.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Please select an item!");
+                return;
+            }
+
+            string itemName = listViewMenu.SelectedItems[0].Text;
+            double price = menu[itemName];
+
+            // Add to Order ListView
+            ListViewItem orderItem = new ListViewItem(itemName);
+            orderItem.SubItems.Add("$" + price.ToString("0.00"));
+            listViewOrder.Items.Add(orderItem);
+
+            // Update total
+            total += price;
+            lblTotal.Text = "Total: $" + total.ToString("0.00");
+
+            // Progress bar
+            progressBar1.Value = Math.Min(progressBar1.Value + 20, 100);
+
+            // Status message
+            toolStripStatusLabel1.Text = itemName + " added!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            private void btnCheckout_Click(object sender, EventArgs e)
+        {
+            if (listViewOrder.Items.Count == 0)
+            {
+                MessageBox.Show("No items in order!");
+                return;
+            }
+
+            // Show receipt
+            string receipt = "***** RECEIPT *****\n\n";
+
+            foreach (ListViewItem item in listViewOrder.Items)
+            {
+                receipt += item.Text + " - " + item.SubItems[1].Text + "\n";
+            }
+
+            receipt += "\nTotal: $" + total.ToString("0.00");
+
+            MessageBox.Show(receipt, "Receipt");
+
+            // Reset everything
+            listViewOrder.Items.Clear();
+            total = 0;
+            lblTotal.Text = "Total: $0.00";
+            progressBar1.Value = 0;
+            toolStripStatusLabel1.Text = "Order completed!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var item in menu)
+            {
+                ListViewItem row = new ListViewItem(item.Key);
+                row.SubItems.Add(item.Value.ToString("0.00"));
+                listViewMenu.Items.Add(row);
+            }
+        }
+    }
+    }
+    }
+    }    
